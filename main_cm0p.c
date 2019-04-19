49eea12d7b2bf8c049e93c57b3ee772cf73dfef7
PeterMankowskiNeuro <47701894+PeterMankowskiNeuro@users.noreply.github.com>
2019-04-18 22:05:22 -0400

    Removed some text

--- main_cm0p.c
+++ main_cm0p.c
@@ -16,9 +16,7 @@
 
 ********************************************************************************
 * Copyright 2019, Novela Neuro.  All rights reserved.
-* You may use this file only in accordance with the license, terms, conditions,
-* disclaimers, and limitations in the end user license agreement accompanying
-* the software package with which this file was provided.
+*
 *******************************************************************************/
 
 #include "project.h"
